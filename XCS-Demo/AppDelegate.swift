cd323d7be295ee77b70a1bb0ef042c61ff27e109
girish potnuru <girishpotnuru@gmail.com>
2018-03-12 22:42:41 +0530

    testeddd

--- XCS-Demo/AppDelegate.swift
+++ XCS-Demo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // overtt Override point for customization after application launch.
+        //pppp overtt Override point for customization after application launch.
         return true
     }
 
